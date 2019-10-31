5b46572d16d2043920a5b88fb459cf322e26701b
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2019-10-31 09:47:53 -0700

    Added Print Statements

--- notes.py
+++ notes.py
@@ -3,3 +3,27 @@ print("Hello World!")
 print(True)
 print(15)
 # print(TRUE) This doesn't work
+
+print(9001)  # This is an Integer
+print(True)  # This is a boolean
+print(9.81)  # This is a float
+print("Hello")  # This is a string
+
+print(5.5)
+print("This is a thing")
+print(False)
+print("Seven")
+print("15")
+
+print("This is a string")
+print("Hello)")
+
+# ----------
+# Math
+# ----------
+print()  # This prints out a blank line
+print()
+print(3 + 5)
+print(5 - 2)
+print(5 * 2)
+print(50 / 5)
