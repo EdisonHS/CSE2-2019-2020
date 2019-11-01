c1c19041e62a6e49db80de1965633280a359ddec
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2019-11-01 13:33:12 -0700

    Added Special Math

--- notes.py
+++ notes.py
@@ -27,3 +27,49 @@ print(3 + 5)
 print(5 - 2)
 print(5 * 2)
 print(50 / 5)
+'''
+This is a multi-line comment.
+
+I can type whatever I want here
+and it does not run
+'''
+
+"""
+This is also a multi-line comment
+"""
+print()  # This prints out a blank line
+print()
+
+# Exponents (Powers)
+print(3 ** 2)  # The answer is 9
+print(6 ** 2)  # The answer is 36
+print(2 ** 3)  # The answer is 8
+print(2 ** 4)  # The answer is 16
+
+# To do a square root: do a power of 1/2
+print(36 ** (1/2))
+
+print()
+print()
+# This is floor division (It rounds down)
+print(16 // 4)  # The answer is 4
+print(15 // 4)  # The answer is 3
+print(13 // 4)  # The answer is 3
+print(12 // 4)  # The answer is 3
+print(7 // 3)  # The answer is 2
+print(-8 // 3)  # The answer is -3
+
+print()
+print()
+# Figure this out (Modulus)
+print(13 % 12)  # The answer is 1
+print(14 % 12)  # The answer is 2
+print(25 % 12)  # The answer is 1
+print(7 % 4)  # The answer is 3
+print(155 % 50)  # The answer is 5
+
+# Mixed Review (Do not run this part)
+print(3 ** 3)  # The answer is 27
+print(18 // 5)  # The answer is 3
+print(13 % 4)  # The answer is 1
+print(21 // 5)  # The answer is 4
