89487a386eb4777ccb596cced529165ec4563090
Mr. Wiebe <MrWiebe@users.noreply.github.com>
2020-01-27 12:38:57 -0800

    Added For Loops

---
+++ For Loops.py
@@ -0,0 +1,48 @@
+for variable in range(10):
+    print("Hello World")
+
+for iterator in range(1500):
+    print("Computer Science")
+
+for i in range(2, 5):
+    print("How many times will this print?")
+
+for i in range(4, 10):
+    print("Magic")
+
+for i in range(8):
+    print(200)
+
+for i in range(5):
+    print(i)
+
+for i in range(12):
+    print(i)
+
+for i in range(10000):
+    print(i)
+
+for i in range(5, 10):
+    print(i)
+
+for i in range(5, 10):
+    print(i + 4)
+
+for i in range(10, 20):
+    print("The number 'i' was:")
+    print(i)
+    print("Now it is:")
+    print(i + 5)
+    print()
+
+for i in range(0, 16, 2):
+    print(i)
+
+for i in range(1, 11, 2):
+    print(i)
+
+for i in range(1, 8, 2):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
